9ab8db527c9a6c882d675cf0c23c3e0a0ffa7c8d
Daniel Fischer <dfischer17htl@outlook.com>
2020-05-25 15:22:40 +0200

    added comment

--- app/src/main/java/com/example/googledriveclient/MainActivity.java
+++ app/src/main/java/com/example/googledriveclient/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Mein Kommentar
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
